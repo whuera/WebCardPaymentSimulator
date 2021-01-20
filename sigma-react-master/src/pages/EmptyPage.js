1839451d6fae591574f67e4417dd8f441d2adda3
Mobilpymes <whuera@gmail.com>
2021-01-20 00:48:06 -0500

    add call api

--- sigma-react-master/src/pages/EmptyPage.js
+++ sigma-react-master/src/pages/EmptyPage.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import { Component, useEffect, useState } from "react";
+import { UserService } from "../service/UserService";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from 'primereact/button';
+
 
 export const EmptyPage = () => {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        const userService = new UserService();
+        userService.getAll().then((data) => setUsers(data));
+    }, []);
 
     return (
-        <div className="p-grid">
-            <div className="p-col-12">
-                <div className="card">
-                    <h5>Empty Page</h5>
-                    <p>Use this page to start from scratch and place your custom content.</p>
-                </div>
-            </div>
+        <div>
+        <div className="card">
+          <DataTable value={users}
+                    paginator rows={5}
+                    header="Header" footer="Footer" className="p-datatable-gridlines" style={{margin: '0 auto', marginTop: '20px'}}>
+            <Column field="id" header="ID"></Column>
+            <Column field="firstName" header="Nombres"></Column>
+            <Column field="lastName" header="Apellidos"></Column>
+          </DataTable>
         </div>
+      </div>
     );
 }
